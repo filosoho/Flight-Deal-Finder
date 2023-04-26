3025d0392fdd5ec372b220c470a6208c034c4487
Anna Stachowska <121820001+filosoho@users.noreply.github.com>
2023-04-26 14:53:54 +0100

    Add files via upload

---
+++ data_manager.py
@@ -0,0 +1,33 @@
+import requests
+import os
+
+SHEETY_PRICES_ENDPOINT = os.environ.get("SHEETY_PRICES_ENDPOINT")
+
+
+class DataManager:
+    """This class is responsible for talking to the Google Sheet."""
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+
+        return self.destination_data
+
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes.
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
